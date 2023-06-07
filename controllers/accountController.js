8e73a4eb883af900ef38d94137f6d9d3c4280fc4
fiona <fionaa.garde@gmail.com>
2023-06-07 09:37:33 +0200

    upadate files account

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,4 +1,59 @@
 const Account = require('../models/accountModel');
+const jwt = require('jsonwebtoken');
+
+ 
+exports.accountRegister = (req, res) => {
+    let newAccount = new Account (req.body);
+
+    newAccount.save((error, account) => {
+        if (error) {
+            res.status(401);
+            console.log(error);
+            res.json({ message: "Reqûete invalide." });
+        }
+        else {
+            res.status(201);
+            res.json({ message: `Utilisateur créé : ${account.email}`});
+        }
+    })
+}
+
+exports.loginRegister = (req, res) => {
+    //find account
+    Account.findOne({email : req.body.email}, (error, account) => {
+        if (error || account == null) {
+            res.status(500);
+            console.log(error);
+            res.json({ message: "Utilisateur inconnu." });
+        }
+        else {
+            //account found
+            if(account.email == req.body.email && account.password == req.body.password) {
+                let accountData = {
+                    id: account._id,
+                    emaim: account.email,
+                    role: "admin"
+                }
+                jwt.sign(accountData, process.env.JWT_KEY, {expiresIn : "30days"}, (error, token) => {
+                    if (error) {
+                        res.status(500);
+                        console.log(error);
+                        res.json({ message: "Impossible de générer le token." });
+                    }
+                    else {
+                        res.status(200);
+                        res.json({token})
+                    }
+                })
+            }
+            else{
+                res.status(401);
+                console.log(error);
+                res.json({message: "Email ou mot de passe incorrect"})
+            }
+        }
+    })
+}
 
 exports.listAllAccounts = (req, res) => {
     Account.find({}, (error, accounts) => {
@@ -13,22 +68,6 @@ exports.listAllAccounts = (req, res) => {
         }
     })
 } 
-
-exports.createAAccount = (req, res) => {
-    let newAccount = new Account(req.body);
-
-    newAccount.save((error, account) => {
-        if (error) {
-            res.status(401);
-            console.log(error);
-            res.json({ message: "Reqûete invalide." });
-        }
-        else {
-            res.status(201);
-            res.json(account);
-        }
-    })
-}
 
 exports.getAAccount = (req, res) => {
     Account.findById(req.params.account_id, (error, account) => {
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,9 +2,12 @@ module.exports = (server) => {
     
     const accountController = require("../controllers/accountController");
     
+
+    server.post("/user/register", userController.userRegister);
+    server.post("/user/login", userController.userLogin);
+    
     server.route("/accounts/:account_id/accounts")
-    .get(accountController.listAllAccounts)
-    .post(accountController.createAAccount);
+    .get(accountController.listAllAccounts);
 
     server.route("/accounts/:account_id") // req.params.account_id
     .get(accountController.getAAccount)
